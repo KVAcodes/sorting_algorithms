03ce30c7dab61bc6625bb7c55a5b03649767e80a
KVAcodes <kadiriavictor@gmail.com>
2022-09-30 22:13:22 +0100

    finished bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ *		 using the Bubble sort algorithm.
+ * @array: the array to sort.
+ * @size: size of the array <array>.
+ *
+ * Return: Nothing(void function).
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t count, count_2, tmp, flag;
+
+	for (count = 0; count < size - 1; count++)
+	{
+		flag = 0;
+		for (count_2 = 0; count_2 < size - count - 1; count_2++)
+		{
+			if (array[count_2] > array[count_2 + 1])
+			{
+				tmp = array[count_2];
+				array[count_2] = array[count_2 + 1];
+				array[count_2 + 1] = tmp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+
+}
