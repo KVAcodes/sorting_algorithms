bfcf61581614b9a188117fa57d4eacec9ebafdab
KVAcodes <kadiriavictor@gmail.com>
2022-10-01 14:14:36 +0100

    added insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ *		      order using the Insertion sort algorithm.
+ * @list: address of the pointer to the first node in the list.
+ *
+ * Return: Nothing(void).
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list, *curr = head->next, *next = curr->next, *back;
+
+	while (curr)
+	{
+		back = curr->prev;
+		while (back && back->n > curr->n)
+		{
+			back->next = curr->next;
+			if (curr->next)
+				curr->next->prev = back;
+			curr->prev = back->prev;
+			if (curr->prev)
+				curr->prev->next = curr;
+			else
+				*list = curr;
+			curr->next = back;
+			back->prev = curr;
+			print_list(*list);
+			back = curr->prev;
+		}
+		curr = next;
+		if (next)
+			next = next->next;
+	}
+}
